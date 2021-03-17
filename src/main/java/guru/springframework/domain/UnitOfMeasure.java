4ac311b1131f613b97768092e9d86f69c37b931c
julien <julien.damond@gmail.com>
2021-03-17 20:42:29 +0100

    finished assignment

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -1,8 +1,18 @@
 package guru.springframework.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
+    private String uom;
 
     public String getDescription() {
         return description;
